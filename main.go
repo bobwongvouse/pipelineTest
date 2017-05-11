e5e01aacf9ee2fc4fb19d1ae330610ed412c5084
tspeech <wj.wong@vouse.co>
2017-05-11 12:09:33 +0800

    Another try

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import("net/http")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-	fmt.Println("And I'm making sure my whole pipeline works on Windows alone.")
 }
 
 func main() {
+
+	fmt.Println("And I'm making sure my whole pipeline works on Windows alone.")
 	fmt.Println("Running.")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
